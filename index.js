60c803dfe56a0f9b71fb78ad930cb09231487ddf
Majaz <majazulhaque123@gmail.com>
2023-02-12 21:04:46 +0530

    build homepage and apply all required css

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 const port = 8000;
+
+app.get('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.static('assets'));
 
 app.listen(port,function(err){
     if(err){
